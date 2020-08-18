c3c36482d56e1c23b9b19f7c6c63c12ca4363d2e
attenbl19 <attenboroughl@protonmail.com>
2020-08-18 16:43:12 -0400

    CRUD .create for user.rb and category.rb

--- bin/run.rb
+++ bin/run.rb
@@ -2,3 +2,28 @@ require_relative '../config/environment'
 
 app = nameOfYourApp.new
 app.run
+
+# Test for user.rb
+barbara = User.create(name: "Barbara")
+frank = User.create(name: "Frank")
+jimmy = User.create(name: "Jimmy")
+lisa = User.create(name: "Lisa")
+josephine = User.create(name: "Josephine")
+stephanie = User.create(name: "Stephanie")
+craig = User.create(name: "Craig")
+mark = User.create(name: "Mark")
+mercedes = User.create(name: "Mercedes")
+jamar = User.create(name: "Jamar")
+
+# Test for category.rb
+
+category = Category.create(name: "Activity")
+category_one = Category.create(name: "Errand")
+category_two = Category.create(name: "Event")
+
+# Test for to_do_list.rb
+
+#tdl = To_Do_List.create(name:, category:, note:, boolean:, loc: )
+tdl_one = 
+tdl_two =
+tdl_ three =
