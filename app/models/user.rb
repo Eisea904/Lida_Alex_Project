7e1f57bea16d3c62e401204ce20859e1610b4115
Alex E(She/Her) <eisea904@newschool.edu>
2020-08-19 11:45:41 -0400

    11:45am Wed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ActiveRecord::Base
   # add associatons!
   has_many :to_do_lists
   has_many :categories, through: :to_do_lists
-
+  
+  def self.registration
+    #put code here...
+  end 
+  
 end
