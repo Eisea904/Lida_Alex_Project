950b943999eb150e60af8344278e0bddc41366a8
Alex E(She/Her) <eisea904@newschool.edu>
2020-08-20 22:14:16 -0400

    spacing clean up in Todolist_app.rb

--- app/models/Todolist_app.rb
+++ app/models/Todolist_app.rb
@@ -11,24 +11,22 @@ class TodolistApp
     end 
 
     def welcome
-      #choice = prompt.select("Welcome to your To-Do-List, how can I help?" ["create user", "log in", "choose category"])
+    #choice = prompt.select("Welcome to your To-Do-List, how can I help?" ["create user", "log in", "choose category"])
 
         prompt.select("Welcome, How can I help you?") do |menu| 
-          menu.choice "Create user", -> {user_registration_helper} 
-          menu.choice "Choose List" , -> {puts "Do you have a list?"}
-          menu.choice "Choose category", -> {create_a_todolist}
+            menu.choice "Create user", -> {user_registration_helper} 
+            menu.choice "Choose List" , -> {puts "Do you have a list?"}
+            menu.choice "Choose category", -> {create_a_todolist}
         end
     end
-
-      
-                        
+           
     def user_registration_helper        
-      userInputValue = User.registration()
-      until userInputValue
-        userInputValue == User.registration()
-      end
-      self.user = userInputValue
-      self.main_menu 
+        userInputValue = User.registration()
+        until userInputValue
+          userInputValue == User.registration()
+        end
+        self.user = userInputValue
+        self.main_menu 
     end 
 
     # def user_login_helper
@@ -39,75 +37,66 @@ class TodolistApp
     #   self.user = user_login_name
     #   self.main_menu 
     # end
-      
 
     def main_menu
+        user.reload        # we reload to get most recent data
+        system "clear"     # clears the terminal and pushes this to the top
+        puts "Hello there #{self.user.name}!"
 
-      user.reload        # we reload to get most recent data
-      system "clear"     # clears the terminal and pushes this to the top
-      puts "Hello there #{self.user.name}!"
-
-      prompt.select("Do you want to..." ) do |menu|
-        menu.choice "Create a To Do List", -> {create_a_todolist}
-        menu.choice "Choose Category", -> {display_category_names}
-      end
+        prompt.select("Do you want to..." ) do |menu|
+          menu.choice "Create a To Do List", -> {create_a_todolist}
+          menu.choice "Choose Category", -> {display_category_names}
+        end
     end
 
     def create_a_todolist
-      selected_category = prompt.select("Select a category") do |menu|
-        Category.all.each do |category|
-          menu.choice "#{category.name}"
-      end
-    end
-      selected_category_id = Category.find_by(name: selected_category).id
+        selected_category = prompt.select("Select a category") do |menu|
+            Category.all.each do |category|
+                menu.choice "#{category.name}"
+            end
+        end
+        selected_category_id = Category.find_by(name: selected_category).id
         print selected_category_id
-
+        #enter variables to make Todolist.new
         entered_loc = prompt.ask("Enter location")
         entered_note = prompt.ask("Enter note")
         entered_completed = prompt.ask("completed? Enter(True/False)")
         entered_name = prompt.ask("Enter title")
         entered_data_for_created_list = Todolist.new(user.name, selected_category, entered_loc, entered_note, entered_completed)
         update_list
-
-        sleep 5
-          self.main_menu
-          # self.main_menu <- To take me back to the main_menu
-      end
-
-      def display_and_update_list
-        # Todolist.all_names => [{name => id}, {name => id}]
-        Todolist.all.each do |todolists|
-          todolists.name
-
-        main_menu
-   
+        # sleep 5
+        #     self.main_menu
+        #     # self.main_menu <- To take me back to the main_menu
     end
 
-end
-
-        def update_to_completed
-          entered_name.entered_completed = True
-          destroy_single_item
+    def display_and_update_list
+        # Todolist.all_names => [{name => id}, {name => id}]
+        Todolist.all.each do |todolists|
+            todolists.name
+            main_menu
         end
+    end
 
-        def update_list
-          prompt.select("Update item to 'completed'?") do |menu|
+    def update_to_completed
+        entered_name.entered_completed = True
+        destroy_single_item
+    end
+
+    def update_list
+        prompt.select("Update item to 'completed'?") do |menu|
             menu.choice "True", -> {update_to_completed}
             menu.choice "False", -> {return "See you later!"}
-          end
         end
+    end
           # choices = {"Barbara" => 1, "Frank" => 2, "Jimmy" => 3, ""}
           # prompt.select("Choose your destiny?", choices)
           #print selected_category.all
 
-          def destroy_single_item
-            prompt.select("Erase completed item?") do |menu|
-              menu.choice "Yes", ->{"deleted completed item, thank you Cha-ching!"}
-              menu.choice "No", -> {"Thank you, Cha-ching!"}
-            end
-          end
-
-    private
-
-  
+    def destroy_single_item
+        prompt.select("Erase completed item?") do |menu|
+            menu.choice "Yes", ->{"deleted completed item, thank you Cha-ching!"}
+            menu.choice "No", -> {"Thank you, Cha-ching!"}
+        end
+    end
+private
 end
